14e71d804919ff4c0dcdddc56f7bc5ffa055b1b5
Craig Trim <craigtrim@gmail.com>
2026-03-13 15:56:15 -0500

    feat: Add get_inflections(), get_morphology(), and get_suffixes() API (Issue #10)

--- gngram_lookup/__init__.py
+++ gngram_lookup/__init__.py
@@ -1,6 +1,9 @@
 """gngram-lookup: Google Ngram frequency lookup."""
 
 from gngram_lookup.data import get_data_dir, get_hash_file, is_data_installed, is_pos_data_installed
+from gngram_lookup.find_suffixes import get_suffixes
+from gngram_lookup.find_inflections import get_inflections
+from gngram_lookup.morphology import Morphology, get_morphology
 from gngram_lookup.lookup import FrequencyData, batch_frequency, erosion_cluster, exists, frequency, prefix_cluster, word_score, wordlist
 from gngram_lookup.pos import PosTag, has_pos, pos, pos_freq
 
@@ -21,4 +24,8 @@ __all__ = [
     "pos_freq",
     "has_pos",
     "PosTag",
+    "get_suffixes",
+    "get_inflections",
+    "Morphology",
+    "get_morphology",
 ]
